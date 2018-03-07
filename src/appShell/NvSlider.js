c4a96d54de40c7f60b37cf18f74824fdbd414c6c
satyam0507@gmail.com <satyam0507@gmail.com>
2018-03-07 20:02:56 +0530

    sub menu removed

--- src/appShell/NvSlider.js
+++ src/appShell/NvSlider.js
@@ -8,43 +8,37 @@ class NvSlider extends Component {
     state = {
         collapsed: this.props.collapsed,
       };
-      componentWillMount(){
-            this.state.collapsed = this.props.collapsed;
-      };
     render(){
         return (
          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
             <div className="logo" style={{minHeight: '84px'}} />
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1">
-            <Link to="/home">
-                <Icon type="pie-chart" />
-                <span>Home</span>
+                <Link to="/home">
+                    <Icon type="pie-chart" />
+                    <span>Home</span>
                 </Link>
             </Menu.Item>
             <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span>Option 2</span>
+                <Link to="/about">
+                    <Icon type="desktop" />
+                    <span>About</span>
+                </Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+                <Link to="/user">
+                    <Icon type="file" />
+                    <span>User</span>
+                </Link>
             </Menu.Item>
             <SubMenu
                 key="sub1"
                 title={<span><Icon type="user" /><span>User</span></span>}
             >
-                <Menu.Item key="3">Tom</Menu.Item>
-                <Menu.Item key="4">Bill</Menu.Item>
-                <Menu.Item key="5">Alex</Menu.Item>
+                <Menu.Item key="4">Tom</Menu.Item>
+                <Menu.Item key="5">Bill</Menu.Item>
+                <Menu.Item key="6">Alex</Menu.Item>
             </SubMenu>
-            <SubMenu
-                key="sub2"
-                title={<span><Icon type="team" /><span>Team</span></span>}
-            >
-                <Menu.Item key="6">Team 1</Menu.Item>
-                <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9">
-                <Icon type="file" />
-                <span>File</span>
-            </Menu.Item>
             </Menu>
         </Sider>
         )
